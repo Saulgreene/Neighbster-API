885ad227b0e94c776595b9fc9df545a6d8e3c9cd
Saul Greene <spdysg@gmail.com>
2017-07-10 16:51:04 -0700

    profile model working

--- model/profile.js
+++ model/profile.js
@@ -11,5 +11,41 @@ const profileSchema = mongoose.Schema({
   userId: {type: mongoose.Schema.Types.ObjectId, ref: 'user'},
 });
 
+profileSchema.pre('save',function(next) {
+  console.log('pre save doc', this);
+  User.findById(this.user)
+    .then(user => {
+      let assignmentIDSet = new Set(user.profile);
+      profileIDSet.add(this._id);
+      user.assignments = Array.from(profileIDSet);
+      return user.save();
+    })
+    .then(() => next())
+    .catch(() =>
+      next(new Error('validation failed to create profile because user does not exist')));
+});
+
+profileSchema.post('save', function(doc, next) {
+  console.log('post save doc', doc);
+  User.findById(doc.user)
+    .then(user => {
+      user.profile.push(doc._id);
+      return user.save();
+    })
+    .then(() => next())
+    .catch(next);
+});
+
+
+profileSchema.post('remove', function(doc, next) {
+  console.log('post remove doc', doc);
+  User.findById(doc.user)
+    .then(user => {
+      user.profile = user.profile.filter(profile => profile._id !== doc._id);
+      return user.save();
+    })
+    .then(() => next())
+    .catch(next);
+});
 
 module.exports = mongoose.model('profile', profileSchema);
--- model/tool.js
+++ model/tool.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 
 const toolSchema = mongoose.Schema({
-  userId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user'},
+  ownerId: {type: mongoose.Schema.Types.ObjectId, required: true, ref: 'user'},
   serialNumber: {type: Number},
   toolName: {type: String, required: true},
   toolDescription: {type: String, required: true},
