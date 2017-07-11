b83d58bdec289c92528710d945959e40435e1fae
Matthew Parker <cadburylion@outlook.com>
2017-07-11 11:57:53 -0700

    Wrote user test passing 200 and clear-db

---
+++ route/user-router.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// npm modules
+const {Router} = require('express');
+const jsonParser = require('body-parser').json();
+
+// app modules
+const basicAuth = require('../lib/basic-auth-middleware.js');
+const User = require('../model/user.js');
+
+// module logic
+const userRouter = module.exports = new Router();
+
+// /api/signup
+userRouter.post('/api/signup', jsonParser, (req, res, next) => {
+  console.log('Hit POST /api/signup');
+  User.create(req.body)
+  .then(console.log('req.body', req.body))
+  .then(token => res.send(token))
+  .catch(next);
+});
