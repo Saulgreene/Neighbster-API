43d3dbe3dcd3c5031211f0663b238e3dca5dfa78
derekcfarmer <derek.farmer@gmail.com>
2017-07-12 13:54:30 -0700

    post matthew merge commit

--- test/tool-test.js
+++ test/tool-test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').config({path: `${__dirname}/../.env`});
+require('dotenv').config({path: `${__dirname}/../.test.env`});
 
 const expect = require('expect');
 const superagent = require('superagent');
